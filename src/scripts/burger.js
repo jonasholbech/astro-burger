ef2defd6fdfa910364083cd90d15e32de7a2794f
Jonas Holbech <jh@jonasholbech.dk>
2023-10-04 20:27:49 +0200

    inline script block

--- src/scripts/burger.js
+++
@@ -1,7 +0,0 @@
-const burgerMenu = document.querySelector("#burger-menu");
-const menu = document.querySelector("#menu");
-
-burgerMenu.addEventListener("click", function () {
-  burgerMenu.classList.toggle("close");
-  menu.classList.toggle("overlay");
-});
